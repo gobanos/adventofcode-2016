5d3f369f0921c25d0734cf710f9fcb8717c1b776
Gregory Obanos <gregory.obanos@gmail.com>
2016-12-12 02:10:55 +0100

    Use a macro to make Screen structure, so I can test the sample !

--- src/day8/problem1.rs
+++ src/day8/problem1.rs
@@ -1,79 +1,102 @@
 use regex::Regex;
 
+macro_rules! make_sized_screen {
+    ($s:ident, width: $w:expr, height: $h:expr) => {
+        use std::fmt;
+
+        struct $s {
+            pixels: [[Pixel; $w]; $h],
+        }
+
+        impl $s {
+            fn rect(&mut self, dim: &Dimension) {
+                assert!(dim.width <= $w);
+                assert!(dim.height <= $h);
+
+                for mut row in self.pixels[0..dim.height].iter_mut() {
+                    for mut pixel in row[0..dim.width].iter_mut() {
+                        *pixel = Pixel::On;
+                    }
+                }
+            }
+
+            fn rotate_row(&mut self, rot: &Rotation) {
+                assert!(rot.index < $h);
+                assert!(rot.offset <= $w);
+
+                let tmp = self.pixels[rot.index].iter().map(|p| *p).collect::<Vec<_>>();
+
+                for (i, &pixel) in tmp.iter().cycle().skip($w - rot.offset).take($w).enumerate() {
+                    self.pixels[rot.index][i] = pixel;
+                }
+            }
+
+            fn rotate_col(&mut self, rot: &Rotation) {
+                assert!(rot.index < $w);
+                assert!(rot.offset <= $h);
+
+                let tmp = self.pixels.iter().map(|r| r[rot.index]).collect::<Vec<_>>();
+
+                for (i, &pixel) in tmp.iter().cycle().skip($h - rot.offset).take($h).enumerate() {
+                    self.pixels[i][rot.index] = pixel;
+                }
+            }
+
+            fn nb_pixel_on(&self) -> usize {
+                self.pixels.iter().flat_map(|row| row.iter()).filter(|&p| *p == Pixel::On).count()
+            }
+        }
+
+        impl Default for $s {
+            fn default() -> Self {
+                $s {
+                    pixels: [[Pixel::Off; $w]; $h],
+                }
+            }
+        }
+
+        impl fmt::Debug for $s {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                for row in self.pixels.iter() {
+                    for pixel in row.iter() {
+                        write!(f, "{}", match *pixel {
+                            Pixel::On => '#',
+                            Pixel::Off => '.',
+                        })?;
+                    }
+                    write!(f, "\n")?;
+                }
+                Ok(())
+            }
+        }
+    }
+}
+
 #[derive(Copy, Clone, PartialEq)]
-enum Pixel {
+pub enum Pixel {
     On,
     Off,
 }
 
-struct Screen {
-    pixels: [[Pixel; 50]; 6],
-}
+make_sized_screen!(Screen, width: 50, height: 6);
 
-impl Screen {
-    fn rect(&mut self, dim: &Dimension) {
-        assert!(dim.width <= 50);
-        assert!(dim.height <= 6);
-
-        for mut row in self.pixels[0..dim.height].iter_mut() {
-            for mut pixel in row[0..dim.width].iter_mut() {
-                *pixel = Pixel::On;
-            }
-        }
-    }
-
-    fn rotate_row(&mut self, rot: &Rotation) {
-        assert!(rot.index < 6);
-        assert!(rot.offset <= 50);
-
-        let tmp = self.pixels[rot.index].iter().map(|p| *p).collect::<Vec<_>>();
-
-        for (i, &pixel) in tmp.iter().cycle().skip(50 - rot.offset).take(50).enumerate() {
-            self.pixels[rot.index][i] = pixel;
-        }
-    }
-
-    fn rotate_col(&mut self, rot: &Rotation) {
-        assert!(rot.index < 50);
-        assert!(rot.offset <= 6);
-
-        let tmp = self.pixels.iter().map(|r| r[rot.index]).collect::<Vec<_>>();
-
-        for (i, &pixel) in tmp.iter().cycle().skip(6 - rot.offset).take(6).enumerate() {
-            self.pixels[i][rot.index] = pixel;
-        }
-    }
-
-    fn nb_pixel_on(&self) -> usize {
-        self.pixels.iter().flat_map(|row| row.iter()).filter(|&p| *p == Pixel::On).count()
-    }
-}
-
-impl Default for Screen {
-    fn default() -> Self {
-        Screen {
-            pixels: [[Pixel::Off; 50]; 6],
-        }
-    }
-}
-
-enum Action {
+pub enum Action {
     Rect(Dimension),
     RotateRow(Rotation),
     RotateColumn(Rotation),
 }
 
-struct Dimension {
+pub struct Dimension {
     width: usize,
     height: usize,
 }
 
-struct Rotation {
+pub struct Rotation {
     index: usize,
     offset: usize,
 }
 
-fn parse_action(input: &str) -> Action {
+pub fn parse_action(input: &str) -> Action {
     lazy_static! {
         static ref RE_RECT: Regex = Regex::new(r"^rect (\d+)x(\d+)$").unwrap();
         static ref RE_ROTATE: Regex = Regex::new(r"^rotate (row|column) [xy]=(\d+) by (\d+)$").unwrap();
@@ -133,4 +156,36 @@ pub fn challenge() -> usize {
     f.read_to_string(&mut input).unwrap();
 
     run(&input)
+}
+
+#[cfg(test)]
+mod test {
+    make_sized_screen!(TestScreen, width: 7, height: 3);
+
+    use super::*;
+
+    fn run_action(screen: &mut TestScreen, action: &Action) {
+        match *action {
+            Action::Rect(ref dim) => screen.rect(dim),
+            Action::RotateRow(ref rot) => screen.rotate_row(rot),
+            Action::RotateColumn(ref rot) => screen.rotate_col(rot),
+        }
+    }
+
+    #[test]
+    fn sample() {
+        let mut screen = TestScreen::default();
+
+        run_action(&mut screen, &parse_action("rect 3x2"));
+        assert_eq!(format!("{:?}", screen), "###....\n###....\n.......\n");
+
+        run_action(&mut screen, &parse_action("rotate column x=1 by 1"));
+        assert_eq!(format!("{:?}", screen), "#.#....\n###....\n.#.....\n");
+
+        run_action(&mut screen, &parse_action("rotate row y=0 by 4"));
+        assert_eq!(format!("{:?}", screen), "....#.#\n###....\n.#.....\n");
+
+        run_action(&mut screen, &parse_action("rotate column x=1 by 1"));
+        assert_eq!(format!("{:?}", screen), ".#..#.#\n#.#....\n.#.....\n");
+    }
 }
